ca25df75715268ee2b4d8156cdd1732945916695
mazaji12 <a01566165@tec.mx>
2023-01-23 18:52:13 -0600

    Final

---
+++ script.js
@@ -0,0 +1,17 @@
+$("Bttnclear").click(TodoClear)
+$("BttnMark").click(TodoMark)
+$("Bttndel").click(TodoDel)
+$("BttnPost").click(TodoPost)
+
+
+function TodoPost(){
+
+    alert("holla")
+}
+
+function TodoClear(){
+
+   
+}
+
+function TodoMark(){}
