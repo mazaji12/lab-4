74508e2537befeb9c2de87f3925de7eae29c7ea2
mazaji12 <a01566165@tec.mx>
2023-01-23 21:03:30 -0600

    Final

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-$("Bttnclear").click(TodoClear)
-$("BttnMark").click(TodoMark)
-$("Bttndel").click(TodoDel)
-$("BttnPost").click(TodoPost)
-
-
-function TodoPost(){
-
-    alert("holla")
-}
-
-function TodoClear(){
-
+$(document).ready(function(){
    
-}
+   
+   
+    $("#BotonPost").click(function(e){
+        e.preventDefault()
+        let todo=$("#todoTxt").val()
+        $("#todoList").append(`<input type="checkbox" name="todo"/> <label> ${todo} </label> <br> `)
+    })
 
-function TodoMark(){}
+    $("#BotonMark").click(function(){
+        let todos= $("[name='todo']")
+        for(let i=0;i<todos.length;i++){
+            todos[i].checked=true
+        }
+       
+    })
+
+    $("#BotonClear").click(function(){
+        let todos= $("[name='todo']")
+        for(let i=0;i<todos.length;i++){
+            todos[i].checked=false
+        }
+       
+    })
+
+    $("#BotonDelete").click(function(){
+        $("#todoList").empty()
+       
+    })
+
+
+    
+
+})
