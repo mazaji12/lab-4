adce2fe1cb3b139c10ae7b604a5a71dfc3475b10
mazaji12 <a01566165@tec.mx>
2023-01-23 18:09:51 -0600

    segundo

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$("Bttnclear").click(TodoClear)
+$("BttnMark").click(TodoMark)
+$("Bttndel").click(TodoDel)
+$("BttnPost").click(TodoPost)
+
+function TodoClear(){
+
+    let todo=$("#Todotxt").val
+    alert(todo)
+}
+
+function TodoMark(){
+    
+}
+function TodoDel(){
+    
+}
+function TodoPost(){
+    
+}
